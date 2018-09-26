15f35492b9abeb44b83fa3bb6f5662ff92ad5880
Hafiz Nurhuda <hafiznurhuda@yahoo.com>
2018-09-26 13:03:31 +0700

    salah
    data

--- TUGAS VB/WindowsApplication1/WindowsApplication1/Form1.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Form1
-
-    Private Sub PembelianToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PembelianToolStripMenuItem.Click
-        FPembeli.Show()
-    End Sub
-End Class
